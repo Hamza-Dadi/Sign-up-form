51bb202a1c3d38ea114b2f82c1948505bf4359ef
Hamza-Dadi <hamzadadi118@gmail.com>
2026-05-16 13:36:57 +0100

    visibility improvement

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ btn.addEventListener("click",function(){
     if(nom.value=="" || email.value=="" || password.value==""){
         alert("veuillez remplir tous les champs");
     }
+    else if(password.value.length<8){
+        alert("Le mot de passe doit contenir au moins 8 caractères");
+    }
+    else{
+        alert("Inscription réussie"+"nom: "+nom.value+" email: "+email.value+" password: "+password.value);
+    }
 })
